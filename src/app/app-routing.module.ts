be9123cb7858ed4af4bff0feb071e0e728624626
ImanZahid <iman.zahid@arksoft.com.tr>
2024-07-08 20:56:24 +0500

    Committing local changes before updating Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { NewEmployeeComponent } from './pages/new-employee/new-employee.component';
+import { EditEmployeeComponent } from './pages/edit-employee/edit-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'new-employee', component: NewEmployeeComponent },
+  { path: 'employee/:id', component: EditEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '**', redirectTo: '/list' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './pages/list/list.component';
+import { NewEmployeeComponent } from './pages/new-employee/new-employee.component';
+import { EditEmployeeComponent } from './pages/edit-employee/edit-employee.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    NewEmployeeComponent,
+    EditEmployeeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AppRoutingModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    CalendarModule,
+    ToastModule,
+    InputNumberModule,
+    CheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
